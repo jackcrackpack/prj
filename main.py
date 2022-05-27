45b13104050ee8b4cd78bda14de5603964d33f68
jackcrackpack <kaoyichieh286@gmail.com>
2022-05-27 18:34:54 +0800

    2

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import cv2 as cv
 
-img = cv.imread('img/cat.jpg')
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+img = cv.imread('img/aks.jpg')   #讀圖片
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)   #轉灰階
 faceCascade = cv.CascadeClassifier('detect.xml')   #載入辨識模型
+imgrect = faceCascade.detectMultiScale(gray, 1.1, 3)   #辨識眼  elon(2.2, 8) ppl(1.1, 5) aks(1.1, 3)
+#參數(掃得圖，每檢測完擴大繼續掃，成功檢測次數(越多次可能仔細，導致可能找不到))
+print(len(imgrect)) #看偵測幾張
+
+for(x, y, w, h) in imgrect:        #可能多部位 用迴圈跑
+    cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+cv.imshow('img', img)     #顯示圖片
 
 cv.waitKey()
